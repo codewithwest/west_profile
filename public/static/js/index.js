80e0bb8f05853c3729576c1a051b3499d9c6e564
West <jonaslekgau@gmail.com>
2023-02-22 10:00:51 +0200

    responsive 450

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,18 +1,17 @@
 
-
+// get the window url
 var currentUrl = window.location.href;
+// get content div from the indexedDB.html file
 var content = document.getElementById('content')
-
+// get the nav links
 var projectLink = document.getElementById('project-link')
 var homeLink = document.getElementById('home-link')
 var aboutLink = document.getElementById('about-link')
 
-
+// Use fetch to get the files required ('home,projects and about')
 let homeCont = await fetch(`./static/views/home.html`).
     then((c) => c.text()).
     catch(e => e)
-
-
 
 let projectCont = await fetch(`./static/views/projects.html`).
     then((c) => c.text()).
@@ -21,16 +20,20 @@ let projectCont = await fetch(`./static/views/projects.html`).
 let aboutCont = await fetch(`./static/views/about.html`).
     then((c) => c.text()).
     catch(e => e)
-console.log()
-if (window.location == 'http://127.0.0.1:5500/public/index.html') {
-    content.innerHTML = projectCont
-}
+
+// Load the home content into the content div when page loads
+// change the home nav link background color
+// set projects and about link background colors to inherit from parent element
 if (window.location.pathname == '/') {
     content.innerHTML = homeCont
     homeLink.style.background="rgba(22,22,22)"
     projectLink.style.background="inherit"
     aboutLink.style.background="inherit"
 }
+// clear the content div
+// Load the home content into the content div when home link is clicked
+// change the home nav link background color
+// set projects and about link background colors to inherit from parent element
 homeLink.addEventListener('click', (e) => {
     e.preventDefault()
     content.innerHTML = ""
@@ -40,15 +43,23 @@ homeLink.addEventListener('click', (e) => {
     aboutLink.style.background="inherit"
 })
 
+// clear the content div
+// Load the projects content into the content div when projects link is clicked
+// change the projects nav link background color
+// set home and about link background colors to inherit from parent element
 projectLink.addEventListener('click', (e) => {
     e.preventDefault()
     content.innerHTML = ""
     content.innerHTML = projectCont
-    
     projectLink.style.background="rgba(22,22,22)"
     homeLink.style.background="inherit"
     aboutLink.style.background="inherit"
 })
+
+// clear the content div
+// Load the about content into the content div when about link is clicked
+// change the about nav link background color
+// set home and projects link background colors to inherit from parent element
 aboutLink.addEventListener('click', (e) => {
     e.preventDefault()
     content.innerHTML = ""
